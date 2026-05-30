aaadadf5d3464c25d1b707ead4cdf5b39a7e6073
ieesparza24-cpu <ie.esparza24@info.uas.edu.mx>
2026-05-30 07:23:04 +0200

    update
    Refactor database connection to support PostgreSQL if DATABASE_URL is set, otherwise fallback to SQLite.

--- db.js
+++ db.js
@@ -1,9 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './db.sqlite'
-});
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite',
+    logging: false
+  });
+}
 
 module.exports = sequelize;
     
